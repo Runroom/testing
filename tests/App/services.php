2d30e25a78dab02cee1391ed16f95303164788f4
Runroom <software@runroom.com>
2022-03-01 09:23:46 +0000

    Merge pull request #122 from Runroom/hotfix/deprecations
    Avoid deprecations on Sonata 4

---
+++ tests/App/services.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Runroom package.
+ *
+ * (c) Runroom <runroom@runroom.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Runroom\Testing\Tests\App\Admin\TestAdmin;
+use Runroom\Testing\Tests\App\Entity\Test;
+use Sonata\AdminBundle\Controller\CRUDController;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    // Use "service" function for creating references to services when dropping support for Symfony 4
+    $services = $containerConfigurator->services();
+
+    $testAdmin = $services->set(TestAdmin::class)
+        ->public()
+        ->tag('sonata.admin', [
+            'model_class' => Test::class,
+            'manager_type' => 'orm',
+            'label' => 'Test Entity',
+        ]);
+
+    /* @todo: Simplify this when dropping support for SonataAdminBundle 3 */
+    if (!is_a(CRUDController::class, AbstractController::class, true)) {
+        $testAdmin->args([null, Test::class, null]);
+    }
+};
